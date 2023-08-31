0a012a5c9a13fad039d3090250fa0ea28842fed4
MaxCMartinsen <max.castoe@outlook.com>
2023-08-31 10:33:03 +0200

    Goofy main screen

--- cursed warzone/Assets/Scripts/GameManager.cs
+++ cursed warzone/Assets/Scripts/GameManager.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public GameObject mainMenu;
+    public GameObject p;
+    public Image backImg;
 
     // Start is called before the first frame update
     void Start()
     {
         mainMenu.SetActive(true);
+        p.GetComponent<playerMovement>().lookSensitivity = 0;
     }
 
     // Update is called once per frame
@@ -18,19 +22,24 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && mainMenu.active)
         {
             mainMenu.SetActive(false);
+            p.GetComponent<playerMovement>().lookSensitivity = 2;
         }
 
-        if (!mainMenu.active && Input.GetKeyDown(KeyCode.Alpha0) || !mainMenu.active && Input.GetKeyDown(KeyCode.Escape))
+        if (!mainMenu.active && Input.GetKeyDown(KeyCode.Alpha0))
         {
             mainMenu.SetActive(true);
+            p.GetComponent<playerMovement>().lookSensitivity = 0;
         }
 
         if (mainMenu.active)
         {
             Time.timeScale = 0.0000001f;
-        } else
+            p.GetComponent<playerMovement>().lookSensitivity = 0;
+        }
+        else
         {
             Time.timeScale = 1;
+            p.GetComponent<playerMovement>().lookSensitivity = 2;
         }
     }
 }
